f01ae3afe755c3c0a2f0ac97b61ff05166f9b959
VXROO <72665810+Varo-Amv@users.noreply.github.com>
2022-01-13 07:52:07 +0700

    Create mess.js

---
+++ VXROMSG/mess.js
@@ -0,0 +1,20 @@
+const prefix = '#'
+module.exports.mess = {
+	        wait: 'Tunggu sebentar...',
+			success: 'Success',
+			wrongFormat: 'Wrong Format!',
+                        limit: `Limit kamu sudah habis\nCek Limit : ${prefix}limit`,
+                        baned: `Nomor kamu telah di banned!\nHubungi member premium untuk membuka banned`,
+			error: {
+				api: 'APINYA HABIS BRO ISI ULANG DULU',
+				stick: 'ITU BUKAN STIKER',
+				Iv: 'Invalid Link!'
+			},
+			only: {
+				group: 'KHUSUS GRUP',
+				admin: 'KHUSUS ADMIN GRUP',
+				premium: 'KHUSUS MEMBER PREMIUM',
+				owner: 'KHUSUS OWNER',
+				Badmin: 'Jadikan bot admin terlebih dahulu!',
+			}
+		}
